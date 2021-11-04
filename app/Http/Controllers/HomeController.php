92971557025247902d033d6a75d7cdd0cc69bc56
Roberto <robertomartinonwg@gmail.com>
2021-11-04 17:03:54 +0100

    modifica della vista home in admin

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,7 +15,7 @@ class HomeController extends Controller
     {
         $this->middleware('auth');
     }
-
+    
     /**
      * Show the application dashboard.
      *
@@ -23,6 +23,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return view('admin');
     }
 }
