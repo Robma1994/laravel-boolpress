ae095b06731b007dd4a384bdc62d580a0948c2d0
Roberto <robertomartinonwg@gmail.com>
2021-11-05 15:16:56 +0100

    Inserimento CREATE

---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.dashboard')
+
+@section('content')
+    <form action="{{ route('admin.posts.store')}}" method="POST">
+        @csrf
+        @method('POST')
+        <div class="mb-3">
+            <label for="title" class="form-label">Title</label>
+            <input type="text" name="title" class="form-control" id="title" placeholder="Add Title">
+        </div>
+        <div class="mb-3">
+            <label for="content" class="form-label">Content</label>
+            <input type="text" name="content" class="form-control" id="content" placeholder="Add content">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+
+@endsection
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -6,6 +6,7 @@
             <h5 class="card-header">{{$post['title']}}</h5>
             <div class="card-body">
             <a href="{{ route('admin.posts.show', $post['id'])}}" class="btn btn-primary">Go details</a>
+            
             </div>
         </div>
     @endforeach
