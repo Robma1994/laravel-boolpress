73f73203710025ddfa29483cb6d5d4077a4717a9
Roberto <robertomartinonwg@gmail.com>
2021-11-05 12:34:32 +0100

    Primo post

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,5 +1,14 @@
-@extends('layouts.dashboard');
+@extends('layouts.dashboard')
 
 @section('content')
-    @dump($posts)
+    @foreach ($posts as $post)
+        <div class="card">
+            <h5 class="card-header">{{$post['title']}}</h5>
+            <div class="card-body">
+            <p class="card-text">{{$post['content']}}</p>
+            <a href="#" class="btn btn-primary">Go details</a>
+            </div>
+        </div>
+    @endforeach
 @endsection
+
