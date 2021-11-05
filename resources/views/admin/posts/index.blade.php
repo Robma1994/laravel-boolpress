e6686d9fc18741cc767b6602dec44ef9f5834a5e
Roberto <robertomartinonwg@gmail.com>
2021-11-05 17:26:45 +0100

    DELETE

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -5,8 +5,13 @@
         <div class="card">
             <h5 class="card-header">{{$post['title']}}</h5>
             <div class="card-body">
-            <a href="{{ route('admin.posts.show', $post['id'])}}" class="btn btn-primary">Go details</a>
-            <a href="{{ route('admin.posts.edit', $post['id'])}}" class="btn btn-warning">Modify Post</a>
+                <a href="{{ route('admin.posts.show', $post['id'])}}" class="btn btn-primary">Go details</a>
+                <a href="{{ route('admin.posts.edit', $post['id'])}}" class="btn btn-warning">Modify Post</a>
+                <form class="d-inline" method="POST" action="{{ route('admin.posts.destroy', $post['id']) }}">
+                    @csrf
+                    @method('DELETE')
+                    <button class="btn btn-danger" type="submit">DELETE</button>
+                </form>
             </div>
         </div>
     @endforeach
