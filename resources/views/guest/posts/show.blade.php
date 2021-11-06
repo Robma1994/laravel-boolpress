ab66e9d50ff7fb3901b80493fac3cc85e956b6cf
Roberto <robertomartinonwg@gmail.com>
2021-11-06 11:50:25 +0100

    Aggiunta del titolo nella card descrizione

--- resources/views/guest/posts/show.blade.php
+++ resources/views/guest/posts/show.blade.php
@@ -5,8 +5,9 @@
     <div class="card">
         <h1 class="card-header">Description</h1>
         <div class="card-body">
-        <p>{{$post['content']}}</p>   
-        <a href="{{ route('posts.index')}}" class="btn btn-primary">Back to list</a>
+            <h1>{{$post['title']}}</h1>
+            <p>{{$post['content']}}</p>   
+            <a href="{{ route('posts.index')}}" class="btn btn-primary">Back to list</a>
         </div>
     </div>
 @endsection
