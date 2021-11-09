91679bc4cda72081752f31fd57aa23d4bc4c47a2
Roberto <robertomartinonwg@gmail.com>
2021-11-09 15:07:22 +0100

    Creazione tabella tags, migrations e seeder

---
+++ database/seeds/TagSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Tag;
+
+class TagSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $tags = ['FrontEnd Dev', 'BackEnd Dev', 'MVC', 'Models' ,'Sedeers'];
+        
+        for($i = 0; $i < count($tags); $i++) {
+            $new_tag = new Tag();
+            $new_tag->name = $tags[$i];
+            $new_tag->slug = Str::slug($tags[$i]);
+            $new_tag->save();
+        }
+    }
+}
--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -31,7 +31,7 @@
                 @endforeach
             </select>
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary">Modify</button>
     </form>
 
 @endsection
